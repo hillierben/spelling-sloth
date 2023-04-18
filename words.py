4a46aee3902e3adc9c59c64e3e489485cdabf3b3
hillierben <113854947+hillierben@users.noreply.github.com>
2023-04-18 19:23:47 +0100

    Add files via upload

---
+++ words.py
@@ -0,0 +1,260 @@
+# Spelling Words
+
+# 3 Letter Words
+threeLetters = (
+    'bye',
+    'sad',
+    'cry',
+    'pig',
+    'rat',
+    'toe',
+    'ink',
+    'eye', 
+    'cat',
+    'sit',
+    'hat', 
+    'eat',
+    'pet',
+    'zip',
+    'kid',
+) 
+
+# 4 Letter Words
+fourLetters = (
+    'busy',
+    'band', 
+    'bark',
+    'barn',
+    'chop',
+    'coat',
+    'cone',
+    'desk',
+    'duck',
+    'farm',
+    'fire',
+    'grow',
+    'home',
+    'jump',
+    'kite',
+    'lift',
+    'leaf'
+)
+
+# 5 Letter Words
+fiveLetters = (
+    'build',
+    'early',
+    'earth',
+    'fruit',
+    'heart',
+    'learn',
+    'often',
+    'woman', 
+    'eight',
+    'bacon',
+    'dream',
+    'happy',
+    'plate', 
+    'stick',
+
+)
+
+# 6 Letter Words
+sixLetters = (
+    'answer',
+    'arrive',
+    'arrive',
+    'caught', 
+    'circle', 
+    'enough', 
+    'famous', 
+    'island',     
+    'minute', 
+
+)
+
+# 7 Letter Words
+sevenLetters = (
+    'believe',
+    'breathe',
+    'bicycle',
+    'certain', 
+    'library', 
+    'popular', 
+    'promise', 
+    'special', 
+    'strange', 
+    'suppose', 
+    'thought', 
+)
+
+# 8 Letter Words
+eightLetters = (
+    'accident',
+    'february',
+    'business',
+    'exercise', 
+    'interest', 
+    'medicine', 
+    'potatoes',  
+    'surprise',
+    'occasion',
+)
+
+# 9 Letter Words
+nineLetters = (
+    'difficult', 
+    'favourite', 
+)
+
+# 10 or more Letter Words
+tenLetters = (
+
+    'blackberry',
+
+    'appearance',
+
+    'gracefully',
+
+    'impossible'
+)
+
+
+audioDict = {
+    # 3 Letter Words
+    'bye': 'static/audio/bye.mp3',
+    'sad': 'static/audio/sad.mp3',
+    'cry': 'static/audio/cry.mp3',
+    'pig': 'static/audio/pig.mp3',
+    'bat': 'static/audio/bat.mp3',
+    'rat': 'static/audio/rat.mp3',
+    'bog': 'static/audio/bog.mp3',
+    'toe': 'static/audio/toe.mp3',
+    'ink': 'static/audio/ink.mp3',
+    'eye': 'static/audio/eye.mp3',
+    'cat': 'static/audio/cat.mp3',
+    'sit': 'static/audio/sit.mp3',
+    'yes': 'static/audio/yes.mp3',
+    'bad': 'static/audio/bad.mp3',
+    'hat': 'static/audio/hat.mp3',
+    'eat': 'static/audio/eat.mp3',
+    'pet': 'static/audio/pet.mp3',
+    'net': 'static/audio/net.mp3',
+    'zip': 'static/audio/zip.mp3',
+    'kid': 'static/audio/kid.mp3',
+
+    # 4 Letter Words
+    'busy': 'static/audio/busy.mp3',
+    'band': 'static/audio/band.mp3',
+    'able': 'static/audio/able.mp3',
+    'bark': 'static/audio/bark.mp3',
+    'barn': 'static/audio/barn.mp3',
+    'chop': 'static/audio/chop.mp3',
+    'coat': 'static/audio/coat.mp3',
+    'cone': 'static/audio/cone.mp3',
+    'date': 'static/audio/date.mp3',
+    'desk': 'static/audio/desk.mp3',
+    'duck': 'static/audio/duck.mp3',
+    'farm': 'static/audio/farm.mp3',
+    'fire': 'static/audio/fire.mp3',
+    'grow': 'static/audio/grow.mp3',
+    'home': 'static/audio/home.mp3',
+    'jump': 'static/audio/jump.mp3',
+    'kite': 'static/audio/kite.mp3',
+    'lift': 'static/audio/lift.mp3',
+    'leaf': 'static/audio/leaf.mp3',
+
+    # 5 Letter Words
+    'build': 'static/audio/build.mp3',
+    'early': 'static/audio/early.mp3',
+    'earth': 'static/audio/earth.mp3',
+    'fruit': 'static/audio/fruit.mp3',
+    'heard': 'static/audio/heard.mp3',
+    'heart': 'static/audio/heart.mp3',
+    'learn': 'static/audio/learn.mp3',
+    'often': 'static/audio/often.mp3',
+    'woman': 'static/audio/woman.mp3',
+    'eight': 'static/audio/eight.mp3',
+    'candy': 'static/audio/candy.mp3',
+    'dream': 'static/audio/dream.mp3',
+    'happy': 'static/audio/happy.mp3',
+    'plate': 'static/audio/plate.mp3',
+    'stick': 'static/audio/stick.mp3',
+
+    # 6 Letter Words
+    'answer': 'static/audio/answer.mp3',
+    'appear': 'static/audio/appear.mp3',
+    'arrive': 'static/audio/arrive.mp3',
+    'caught': 'static/audio/caught.mp3',
+    'circle': 'static/audio/circle.mp3',
+    'enough': 'static/audio/enough.mp3',
+    'famous': 'static/audio/famous.mp3', 
+    'island': 'static/audio/island.mp3',
+    'minute': 'static/audio/minute.mp3',
+
+    # 7 Letter Words
+    'believe': 'static/audio/believe.mp3',
+    'bicycle': 'static/audio/bicycle.mp3',
+    'breathe': 'static/audio/breathe.mp3',
+    'certain': 'static/audio/certain.mp3',
+    'library': 'static/audio/library.mp3',
+    'popular': 'static/audio/popular.mp3',
+    'special': 'static/audio/special.mp3',
+    'strange': 'static/audio/strange.mp3',
+    'suppose': 'static/audio/suppose.mp3',
+    'thought': 'static/audio/thought.mp3',
+
+    # 8 Letter Words
+    'accident': 'static/audio/accident.mp3',
+    'business': 'static/audio/business.mp3',
+    'exercise': 'static/audio/exercise.mp3',
+    'february': 'static/audio/february.mp3',
+    'interest': 'static/audio/interest.mp3',
+    'medicine': 'static/audio/medicine.mp3',
+    'occasion': 'static/audio/occasion.mp3',
+    'potatoes': 'static/audio/potatoes.mp3',
+    'surprise': 'static/audio/surprise.mp3',
+
+
+    # 9 Letter Words
+    'different': None, 
+    'difficult': 'static/audio/difficult.mp3',
+    'disappear': None,
+    'favourite': 'static/audio/favourite.mp3',
+    'important': None,
+    'knowledge': None,
+    'therefore': None,
+    'anonymous': None,
+    'exception': None,
+    'guarantee': None,
+    'happiness': None,
+    'excellent': None,
+    'necessary': None,
+    'influence': None,
+    'marketing': None,
+    'agreement': None,
+    'advantage': None,
+    'intention': None,
+    'permanent': None,
+
+    # 10 or more Letter Words
+    'accidentally': None,
+    'experience': None,
+    'experiment': None,
+    'particular': None,
+    'possession': None,
+    'occasionally': None,
+    'difficulty': None,
+    'confidence': None,
+    'friendship': None,
+    'generation': None,
+    'blackberry': 'static/audio/blackberry.mp3',
+    'background': None,
+    'babysitter': None,
+    'commitment': None,
+    'appearance': 'static/audio/appearance.mp3',
+    'accomplish': None,
+    'gracefully': 'static/audio/gracefully.mp3',
+    'industrial': None,
+    'impossible': 'static/audio/impossible.mp3',
+}
